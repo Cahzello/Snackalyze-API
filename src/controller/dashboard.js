57b34a7bd6baf88843d2d5944909c961444df3a2
Cahzello <rizkyrasya.m@gmail.com>
2024-12-10 22:06:12 +0700

    changin getAllergy fn to more verbose

--- src/controller/dashboard.js
+++ src/controller/dashboard.js
@@ -1,10 +1,9 @@
-const {readAllergy} = require("../models/Allergy");
+const { readAllergy } = require("../models/Allergy");
 
-const dashboard = async (req, res) => {
-    const userId = req.user.id;
-    const allergy = await readAllergy(userId);
-    console.log(allergy)
-    return allergy;
-}
+const getAllergy = async (req, res) => {
+  const userId = req.user.id;
+  const allergy = await readAllergy(userId);
+  return allergy;
+};
 
-module.exports = dashboard; 
+module.exports = getAllergy;
--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const dashboard = require("../controller/dashboard");
+const getAllergy = require("../controller/dashboard");
 const { authenticateToken } = require("../middleware/Authorizatoin");
 const response = require("../skemaResponse");
 
 router.get("/", authenticateToken, async (req, res) => {
-  const data = await dashboard(req, res);
+  const data = await getAllergy(req, res);
   response(res, { status: 200, message: "Success", payload: data });
 });
 
